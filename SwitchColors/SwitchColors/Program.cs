8769edab0527e672afe4866399da36cba85a005a
seq0h <tihomirovarseni@gmail.com>
2025-10-17 11:43:16 +0300

    SwitchColors

---
+++ SwitchColors/SwitchColors/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SwitchColors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+             //kasutada switchi
+             //kuvab iga kord suvalised värvid
+             //Red, Green, Blue, White, Orange
+             //iga värvi puhul näitab vastava värviga konsooli
+        }
+    }
+}
