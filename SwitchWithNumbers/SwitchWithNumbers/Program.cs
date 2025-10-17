9cbcbafa236bf4e77aa8365e2e7113be1e1f75a9
seq0h <tihomirovarseni@gmail.com>
2025-10-17 11:23:25 +0300

    SwitchWithNumbers

---
+++ SwitchWithNumbers/SwitchWithNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace SwitchWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Teie töö on teha switch rakendus,
+            //kus on kolm case (1, 2, 3)
+            //Iga case puhul teeb erinev arv beep-e
+
+            Console.Write("Sisesta number (1, 2 või 3): ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int number)) ;
+
+
+            switch (number)
+            {
+                case 1:
+                    Console.Beep(1000,500);
+                    break;
+                case 2:
+                    Console.Beep(2000,500);
+                    Console.Beep(2000,500);
+                    break;
+                case 3:
+                    Console.Beep(3000,500);
+                    Console.Beep(3000, 500);
+                    Console.Beep(3000, 500);
+                    break;
+                default:
+                    Console.WriteLine("Palun sisesta number 1, 2 või 3.");
+                    break;
+
+
+
+            }
+        }
+    }
+}
