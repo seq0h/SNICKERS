878fc6711c5e682817b66ee4db1ec1bc5a86a3f8
seq0h <tihomirovarseni@gmail.com>
2025-10-17 09:23:25 +0300

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+namespace Switch;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Sisesta vokaal ja vajuta enter");
+        string input = Console.ReadLine();
+
+        switch (input)
+        {
+            case "a":
+                Console.WriteLine("Vokaal a");
+                break;
+
+            case "e":
+                Console.WriteLine("Vokaal e");
+                break;
+
+            case "i":
+                Console.WriteLine("Vokaal i");
+                break;
+
+            default:
+                Console.WriteLine("Muu täht");
+
+               break;
+        }
+    }
+}
